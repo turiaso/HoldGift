1d69f2fc1c43dbf082577cae80041a80a18acca7
Alien-Potatil\Alien <turiaso13@hotmail.com>
2016-12-17 23:33:37 +0100

    Generador de Bloques

---
+++ Assets/scripts/GiftGenerator.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+public var itemGen:GameObject;
+
+var timeElapse:System.Double = 0;
+function Start () {
+	
+}
+
+function Update () {
+    Debug.Log("Update time :" + Time.deltaTime);
+    timeElapse+= Time.deltaTime;
+    if(timeElapse>1){
+        var pos = Vector3 (-5, 6, 0);
+        Instantiate(itemGen, pos, Quaternion.identity);
+        timeElapse=0;
+    }
+    Debug.Log(timeElapse);
+}
