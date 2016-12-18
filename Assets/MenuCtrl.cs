08a4a1c96b82941f6eb75cf5274e493b47404de2
EQUIPO\keiko <elaryan13@gmail.com>
2016-12-18 11:20:59 +0100

    no message

---
+++ Assets/MenuCtrl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class MenuCtrl : UnityEngine.MonoBehaviour
+{
+
+	public void loadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
