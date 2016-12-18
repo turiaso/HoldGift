bc2a2ddb4aba38c76d0bceee4882eee388a11def
Alien-Potatil\Alien <turiaso13@hotmail.com>
2016-12-18 12:13:10 +0100

    no message

--- Assets/scripts/GiftGenerator.js
+++ Assets/scripts/GiftGenerator.js
@@ -1,8 +1,10 @@
 ﻿#pragma strict
 
 public var itemsGen:  GameObject[];
+public var maxGifts:int;
 
 private var timeElapse:System.Double = 0;
+private var gifts:int = 0;
 
 function Start () {
 	
@@ -10,7 +12,7 @@ function Start () {
 
 function Update () {
     timeElapse+= Time.deltaTime;
-    if(timeElapse>3f){
+    if(timeElapse>3f && gifts<maxGifts){
         var randomType = Mathf.FloorToInt(Random.Range( 0.0f,itemsGen.length+1));
         if(randomType>=itemsGen.length){
             randomType=0;
@@ -21,5 +23,14 @@ function Update () {
         var pos = Vector3 (x, y, z);
         Instantiate(itemsGen[randomType], pos, Quaternion.identity);
         timeElapse=0;
+        gifts++;
     }
+}
+
+function getGifts(){
+    return gifts;
+}
+
+function setGifts(_gifts:int){
+    gifts = -gifts;
 }
