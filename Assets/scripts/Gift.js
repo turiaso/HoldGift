63dd9b3afc626ad48f7de0a62e426a73ad62cde9
Alien-Potatil\Alien <turiaso13@hotmail.com>
2016-12-18 01:34:05 +0100

    no message

--- Assets/scripts/Gift.js
+++ Assets/scripts/Gift.js
@@ -2,11 +2,26 @@
 
 public var type: int;
 private var onFloor: System.Boolean = false;
+private var timeOnFloor:System.Double = 0;
 
 function Start () {
 	
 }
 
 function Update () {
-	
+    if(onFloor){
+        timeOnFloor+=Time.deltaTime;
+        if( timeOnFloor>1){
+            Destroy(gameObject);
+        }
+    }
+}
+
+//function OnCollisionEnter(collision : Collision) {
+//    Destroy(collision.gameObject);
+//}
+
+function OnCollisionEnter(collision : Collision) {
+    //if(collision.gameObject.tag == "Destroyer")    
+    onFloor = true;
 }
--- Assets/scripts/GiftGenerator.js
+++ Assets/scripts/GiftGenerator.js
@@ -2,7 +2,7 @@
 
 public var itemsGen:  GameObject[];
 
-var timeElapse:System.Double = 0;
+private var timeElapse:System.Double = 0;
 
 function Start () {
 	
@@ -10,12 +10,10 @@ function Start () {
 
 function Update () {
     timeElapse+= Time.deltaTime;
-    if(timeElapse>1){
+    if(timeElapse>1.5f){
         var randomType = Mathf.FloorToInt(Random.Range( 0.0f,itemsGen.length+1));
-        Debug.Log("randomType :" + randomType);
         var pos = Vector3 (-5, 6, 0);
         Instantiate(itemsGen[randomType], pos, Quaternion.identity);
         timeElapse=0;
     }
-    Debug.Log(timeElapse);
 }
